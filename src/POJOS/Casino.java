cc9171fc9e40b66d93935005dd5d3803c586a0b5
MiguelAV22 <miguel.andresvelasco@usp.ceu.es>
2023-03-31 19:09:20 +0200

    ssdsd

--- src/POJOS/Casino.java
+++ src/POJOS/Casino.java
@@ -2,14 +2,8 @@
 package POJOS;
 
 public class Casino {
-	
-<<<<<<< HEAD
-	String Maikol = new String ("como molo");
-
-=======
-	int a;
->>>>>>> branch 'master' of https://github.com/JaviBlazquez/Bill-etes.git
+ branch 'master' of https://github.com/JaviBlazquez/Bill-etes.git
 	String name;
 	///testing
-
+///HOLA SE ME ESCUCHA?
 }
