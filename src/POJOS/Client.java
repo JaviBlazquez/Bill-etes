7911bb03a8728ed0a5280a8299b27a6665575d9d
MiguelAV22 <miguel.andresvelasco@usp.ceu.es>
2023-04-14 17:02:23 +0200

    Setters

--- src/POJOS/Client.java
+++ src/POJOS/Client.java
@@ -7,6 +7,7 @@ public class Client {
 	private String name;
 	private String surname;
 	private boolean condition;
+	
 	public Client(int clientId, int phone, float money, String name, String surname, boolean condition) {
 		
 		this.clientId = clientId;
@@ -14,6 +15,54 @@ public class Client {
 		this.money = money;
 		this.name = name;
 		this.surname = surname;
+		this.condition = condition;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public float getMoney() {
+		return money;
+	}
+
+	public void setMoney(float money) {
+		this.money = money;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public boolean isCondition() {
+		return condition;
+	}
+
+	public void setCondition(boolean condition) {
 		this.condition = condition;
 	}
 	
