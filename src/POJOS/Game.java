db26e011cef3ef4083b828cd9b89ed245089d589
MiguelAV22 <miguel.andresvelasco@usp.ceu.es>
2023-04-14 17:21:33 +0200

    .

--- src/POJOS/Game.java
+++ src/POJOS/Game.java
@@ -2,4 +2,37 @@ package POJOS;
 
 public class Game {
 
+	private int clientId;
+	private int machineId;
+	private int timeStamp;
+	
+	
+	
+	public Game(int clientId, int machineId, int timeStamp) {
+		super();
+		this.clientId = clientId;
+		this.machineId = machineId;
+		this.timeStamp = timeStamp;
+	}
+	
+	public int getClientId() {
+		return clientId;
+	}
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+	public int getMachineId() {
+		return machineId;
+	}
+	public void setMachineId(int machineId) {
+		this.machineId = machineId;
+	}
+	public int getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(int timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
 }
--- src/POJOS/Match.java
+++ src/POJOS/Match.java
@@ -1,5 +1,43 @@
 package POJOS;
 
 public class Match {
+	
+	private int clientId;
+	private int tableId;
+	private int timeStamp;
+	
+	public Match(int clientId, int tableId, int timeStamp) {
+		super();
+		this.clientId = clientId;
+		this.tableId = tableId;
+		this.timeStamp = timeStamp;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	public int getTableId() {
+		return tableId;
+	}
+
+	public void setTableId(int tableId) {
+		this.tableId = tableId;
+	}
+
+	public int getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(int timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
+	
 
 }
