728b03ac7c319ddc79f42a28eccc0a0d5c869966
Alaldb <alejandro.alvarezbenito@usp.ceu.es>
2023-04-14 17:40:19 +0200

    error

---
+++ src/POJOS/Shift.java
@@ -0,0 +1,38 @@
+package POJOS;
+
+public class Shift {
+
+	private int tableId;
+	private int croupierId;
+	private int timeStamp;
+	
+	
+	
+	public Shift(int tableId, int croupierId, int timeStamp) {
+		super();
+		this.tableId = tableId;
+		this.croupierId = croupierId;
+		this.timeStamp = timeStamp;
+	}
+	
+	public int getTableId() {
+		return tableId;
+	}
+	public void setTableId(int tableId) {
+		this.tableId = tableId;
+	}
+	public int getCroupierId() {
+		return croupierId;
+	}
+	public void setCroupierId(int croupierId) {
+		this.croupierId = croupierId;
+	}
+	public int getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(int timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
+}
