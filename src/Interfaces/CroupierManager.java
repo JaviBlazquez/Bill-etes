e12294d59588e715ea32a01c6a6e4448afbbe4c9
migue <miguel.roblesuriel@usp.ceu.es>
2023-04-14 16:39:56 +0200

    jdbcs

---
+++ src/Interfaces/CroupierManager.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface CroupierManager extends WorkerManager{
+	public void assignTable(int croupierId, int tableId);
+}
---
+++ src/Interfaces/WorkerManager.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import java.util.List;
+import POJOS.Worker;
+
+public interface WorkerManager {
+	public void addWorker(Worker w);
+	public List<Worker> getListOfWorkers();
+	public void updateWorker(Worker w);
+	public void removeWorker(Worker w);
+}
