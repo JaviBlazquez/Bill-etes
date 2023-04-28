a4c9cf99a3ea5f4e3301e3bacc1679049752bc29
MiguelAV22 <miguel.andresvelasco@usp.ceu.es>
2023-04-28 19:54:54 +0200

    About the Casino UI and the menu

---
+++ src/CasinoUI/Menu.java
@@ -0,0 +1,22 @@
+package CasinoUI;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+import Interfaces.*;
+import POJOS.*;
+import jdbc.*;
+public class Menu {
+	
+	private static BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+	
+	public static void main(String[] args) {
+		
+		
+	
+	}
+	
+	
+	
+	
+}
