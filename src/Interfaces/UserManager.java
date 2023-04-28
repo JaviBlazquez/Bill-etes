5ff81b48fe1af6a69ce83d11d62304ac48aabde1
migue <miguel.roblesuriel@usp.ceu.es>
2023-04-28 19:53:28 +0200

    jpa

---
+++ src/Interfaces/UserManager.java
@@ -0,0 +1,18 @@
+package Interfaces;
+
+import java.util.List;
+
+import POJOS.Role;
+import POJOS.User;
+
+public interface UserManager {
+	public void disconnect();
+	public void newUser(User u);
+	public void newRole(Role r);
+	
+	public Role getRole(String email);
+	
+	public List<Role> getRoles();
+	
+	public User checkPassword(String email, String passwd);
+}
