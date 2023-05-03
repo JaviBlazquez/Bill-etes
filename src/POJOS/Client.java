fca12327b42a1ca586506600a79a923e719e8c9c
MiguelAV22 <miguel.andresvelasco@usp.ceu.es>
2023-05-03 20:27:18 +0200

    XML de client

--- src/POJOS/Client.java
+++ src/POJOS/Client.java
@@ -2,16 +2,34 @@ package POJOS;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlAccessType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "Client")
+@XmlType(propOrder = {"phone", "money", "condition"})
 public class Client implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -9034033102536229349L;
+	
+	@XmlTransient
 	private int clientId;
+	@XmlElement
 	private int phone;
+	@XmlElement
 	private float money;
+	@XmlAttribute
 	private String name;
+	@XmlAttribute
 	private String surname;
+	@XmlElement
 	private boolean condition;
 	public Client(int clientId, int phone, float money, String name, String surname, boolean condition) {
 		
